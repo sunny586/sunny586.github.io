bb7398ed9ac21183209354c23e1d2a71b87ee568
sunny586 <313283928@qq.com>
2015-11-27 14:08:58 +0800

    zhangyu

--- warehouse/app/scripts/controllers/pages/commonMenuCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('app')
-	.controller('commonMenuCtrl', ['$scope', '$location', 'authService', 'popupService','userInfo',
-		function($scope, $location, authService, popupService,userInfo) {
-			$scope.logout = function() {
-				userInfo.clear();
-				$location.path('/access/signin');
-			}
-		}
-	]);
