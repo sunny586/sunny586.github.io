0fe9aa0b6ba0f2ff7306f7aa578362cfb591278b
zhangyu <313283928@qq.com>
2016-07-02 17:47:23 +0800

    aa

--- react/bundle.js
+++ react/bundle.js
@@ -36840,42 +36840,52 @@
 							_react2['default'].createElement(
 								'p',
 								null,
-								'1.2011年7月毕业于武汉电力职业技术学院，2013年8月份开始前端开发工作。有坚实的前端开发基础，喜欢学习尝试新的技术。'
+								'1.通过DIV+CSS和W3C标准，能熟练运用HTML，CSS及html5，css3进行网页制作，理解各主流浏览器之间的兼容性;'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'2.熟练掌握javaScript,jquery,jquery mobile,ajax等前端技术。'
+								'2.熟悉掌握javascript , jquery , Angular1.x , Ionic , Angular2.0 , TypeScript , Vue, React ;'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'3.能够独立的运用AngularJS +Bootstrap+Gulp搭建一套前端开发环境框架，以及嵌套路由配 置，并已积累一些常用的指令，基本可以满足一般的开发需求。'
+								'3.善于使用前端的基本调试工具和Git,GitLab，Github , sourceTree , bitbucket , jira项目管理工具以及Glup，webpack等项目构建打包工具；'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'4.熟练使用React+ES6+Webpack，可以完成React项目的开发工作，可以独立的运用React编写常用的组件'
+								'4.能够独立的运用AngularJS(1.x) +Bootstrap+Gulp搭建一套前端开发环境框架，以及嵌套路由配 置，并已积累一些常用的指令，基本可以满足一般的开发需求；'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'5.能够使用Ionic开发APP，可以独立配置Android 的SDK+ADT的环境配置，能够将项目打包apk。'
+								'5.熟练使用React+ES6+Webpack，可以完成React项目的开发工作，可以独立的运用React编写常用的组件；'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'6.擅于使用前端的基本调试工具以及Git,GitLab，Github ,sourceTree,bitbucket,jira项目管理工具。'
+								'6.能够使用Ionic开发APP，可以独立配置Android 的SDK+ADT的环境配置，能够将项目打包apk；'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'7.熟悉d3.js数据可视化。'
+								'7.熟悉d3.js数据可视化；'
 							),
 							_react2['default'].createElement(
 								'p',
 								null,
-								'8.对web前端有着强烈的兴趣，能到web前沿技术研究和新技术调研，有良好的学习能力和团队合作精神。'
+								'8.了解Java编程及相关技术；'
+							),
+							_react2['default'].createElement(
+								'p',
+								null,
+								'9.具有一定的文档交付能力，能够独立解决具体的技术问题；'
+							),
+							_react2['default'].createElement(
+								'p',
+								null,
+								'10.对Web前端技术有强烈兴趣，能对Web前沿技术研究和新技术调研，有良好的学习能力和团队合作精神。'
 							),
 							_react2['default'].createElement(
 								'p',
@@ -36932,7 +36942,7 @@
 
 
 	// module
-	exports.push([module.id, ".section-title.center hr{height:4px;width:70px;text-align:center;position:relative;background:#E87E04;margin:0 auto;margin-bottom:30px;border:0;}\r\n#about img{border-radius:50%;width:200px;height:200px;display:inline-block;}\r\n#about p{margin-top:10px;}", ""]);
+	exports.push([module.id, ".section-title.center hr{height:4px;width:70px;text-align:center;position:relative;background:#E87E04;margin:0 auto;margin-bottom:30px;border:0;}\r\n#about img{border-radius:50%;width:200px;height:200px;display:inline-block;}\r\n#about p{margin-top:10px;font-size:14px;font-family:cursive;}", ""]);
 
 	// exports
 
@@ -37303,7 +37313,7 @@
 		end: '至今',
 		company: '深圳百分点',
 		job: 'web前端开发工程师',
-		work: '负责公司后台管理系统以及组件开发'
+		work: '百分点个性化推荐系统BPS以及TCL老门店系统等'
 	}, {
 		startTime: '2015.04',
 		end: '2015.11',
