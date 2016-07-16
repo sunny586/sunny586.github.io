b1839afc20188a347096c5e0c3a4bb4ed660781e
zhangyu <313283928@qq.com>
2016-07-17 00:07:22 +0800

    zhangyu

--- ng1seedzy/app/scripts/controllers/component/uploadCtrl.js
+++ ng1seedzy/app/scripts/controllers/component/uploadCtrl.js
@@ -7,7 +7,7 @@ angular.module('app')
 	.controller('uploadCtrl', ['$scope', 'FileUploader', function($scope, FileUploader) {
 
 		var uploader = $scope.uploader = new FileUploader({
-			url: 'scripts/controllers/upload.php'
+			url: 'https://angular-file-upload-cors-srv.appspot.com/upload'
 		});
 		
 		// FILTERS
@@ -25,43 +25,45 @@ angular.module('app')
 			}
 		});
 
-		// CALLBACKS
-		uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
-			console.info('onWhenAddingFileFailed', item, filter, options);
-		};
-		uploader.onAfterAddingFile = function(fileItem) {
-			console.info('onAfterAddingFile', fileItem);
-		};
+		// // CALLBACKS
+		// uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
+		// 	console.info('onWhenAddingFileFailed', item, filter, options);
+		// };
+		// uploader.onAfterAddingFile = function(fileItem) {
+		// 	console.info('onAfterAddingFile', fileItem);
+		// };
 
-		uploader.onAfterAddingAll = function(addedFileItems) {
-			console.info('onAfterAddingAll', addedFileItems);
-		};
-		uploader.onBeforeUploadItem = function(item) {
-			console.info('onBeforeUploadItem', item);
-		};
-		uploader.onProgressItem = function(fileItem, progress) {
-			console.info('onProgressItem', fileItem, progress);
-		};
-		uploader.onProgressAll = function(progress) {
-			console.info('onProgressAll', progress);
-		};
+		// uploader.onAfterAddingAll = function(addedFileItems) {
+		// 	console.info('onAfterAddingAll', addedFileItems);
+		// };
+		// uploader.onBeforeUploadItem = function(item) {
+		// 	console.info('onBeforeUploadItem', item);
+		// };
+		// uploader.onProgressItem = function(fileItem, progress) {
+		// 	console.info('onProgressItem', fileItem, progress);
+		// };
+		// uploader.onProgressAll = function(progress) {
+		// 	console.info('onProgressAll', progress);
+		// };
 		uploader.onSuccessItem = function(fileItem, response, status, headers) {
-			console.info('onSuccessItem', fileItem, response, status, headers);
-		};
-		uploader.onErrorItem = function(fileItem, response, status, headers) {
-			console.info('onErrorItem', fileItem, response, status, headers);
-		};
-		uploader.onCancelItem = function(fileItem, response, status, headers) {
-			console.info('onCancelItem', fileItem, response, status, headers);
-		};
-		uploader.onCompleteItem = function(fileItem, response, status, headers) {
-			console.info('onCompleteItem', fileItem, response, status, headers);
-		};
-		uploader.onCompleteAll = function() {
-			console.info('onCompleteAll');
-		};
+			//console.info('onSuccessItem', fileItem, response, status, headers);
 
-		console.info('uploader', uploader);
+			console.log(response)
+		};
+		// uploader.onErrorItem = function(fileItem, response, status, headers) {
+		// 	console.info('onErrorItem', fileItem, response, status, headers);
+		// };
+		// uploader.onCancelItem = function(fileItem, response, status, headers) {
+		// 	console.info('onCancelItem', fileItem, response, status, headers);
+		// };
+		// uploader.onCompleteItem = function(fileItem, response, status, headers) {
+		// 	console.info('onCompleteItem', fileItem, response, status, headers);
+		// };
+		// uploader.onCompleteAll = function() {
+		// 	console.info('onCompleteAll');
+		// };
+
+		// console.info('uploader', uploader);
 
 
 
